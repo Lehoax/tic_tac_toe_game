1c0440efa3b13a832f37ad679891340982805428
Lehoax <leolair.pro@gmail.com>
2021-04-29 12:45:17 -0700

    correction bug replace

--- app.rb
+++ app.rb
@@ -10,8 +10,14 @@ nb = 0
 while game.end_game != true 
     game.round
     nb += 2.25
-   if  nb == 9
-    puts "match nul"
+   if  nb > 9
+    system "clear"
+    puts "                       _                    _ "
+    puts "             _        | |                  | |"
+    puts " ____   ____| |_  ____| | _     ____  _   _| |"
+    puts "|    \\ / _  |  _)/ ___) || \\   |  _ \\| | | | |"
+    puts "| | | ( ( | | |_( (___| | | |  | | | | |_| | |"
+    puts "|_|_|_|\\_||_|\\___)____)_| |_|  |_| |_|\\____|_|"
        break
    end
 end
--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,11 @@ class Board
         @board = {":a1"=>" ", ":b1" => " ", ":c1" => " ", ":a2" => " ", ":b2" => " ", ":c2" => " ", ":a3" => " ", ":b3" => " ", ":c3" => " "}
     end
     def put_symbole(location, symbole)
-        @board[":#{location}"] = symbole
+
+        if  @board[":#{location}"].include?(" ")
+            @board[":#{location}"] = symbole
+        end
+        
     end
+   
 end
